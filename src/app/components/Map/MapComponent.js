e19920988ece2dfa21f2e38231949d010b78cbab
1amKhush <khushvendras99@gmail.com>
2025-02-26 19:44:16 +0530

    initialized leaflet

--- src/app/components/Map/MapComponent.js
+++ src/app/components/Map/MapComponent.js
@@ -1,13 +1,13 @@
-"use client"; // Ensures this component runs only on the client side
+"use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useEffect, useState } from "react";
+import styles from "./MapComponent.module.css";
 
-// Fix missing Leaflet marker icons
 const customIcon = new L.Icon({
-  iconUrl: "/marker-icon-2x.png", // Load from public folder
+  iconUrl: "/marker-icon-2x.png",
   shadowUrl: "/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
@@ -20,11 +20,18 @@ const MapComponent = () => {
     setMounted(true);
   }, []);
 
-  if (!mounted) return <p>Loading map...</p>;
+  if (!mounted) return <p className={styles.loadingText}>Loading map...</p>;
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "500px", width: "100%" }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      className={styles.mapContainer}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
       <Marker position={[51.505, -0.09]} icon={customIcon}>
         <Popup>Welcome to this location!</Popup>
       </Marker>
@@ -32,4 +39,4 @@ const MapComponent = () => {
   );
 };
 
-export default MapComponent;
+export default MapComponent;
